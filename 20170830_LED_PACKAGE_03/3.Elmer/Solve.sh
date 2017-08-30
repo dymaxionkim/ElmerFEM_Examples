4820fc45a4e07ecad6e62baf4b0c2f6ce2d67013
dymaxionkim <dymaxion.kim@gmail.com>
2017-08-30 16:22:18 +0900

    LED Thermal

---
+++ 20170830_LED_PACKAGE_03/3.Elmer/Solve.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ElmerSolver case1.sif > case1.log &
+ElmerSolver case2.sif > case2.log &
+ElmerSolver case3.sif > case3.log &
+
+ElmerSolver case4.sif > case4.log &
+ElmerSolver case5.sif > case5.log &
+ElmerSolver case6.sif > case6.log &
+
+ElmerSolver case7.sif > case7.log &
+ElmerSolver case8.sif > case8.log &
+ElmerSolver case9.sif > case9.log &
+
+exit 0
