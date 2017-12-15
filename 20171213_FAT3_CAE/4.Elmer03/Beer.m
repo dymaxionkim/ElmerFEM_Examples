849df3bdac9b4de97ae22461a3073e6e5a7a5135
dymaxionkim <dymaxion.kim@gmail.com>
2017-12-15 21:26:46 +0900

    Fix Errors of Units

--- 20171213_FAT3_CAE/4.Elmer03/Beer.m
+++ 20171213_FAT3_CAE/4.Elmer03/Beer.m
@@ -8,7 +8,7 @@ Depth_Dermis = 0.0015;
 Depth_Fat = 0.005;
 Depth_Muscle = 0.020;
 Width = 0.06;
-Segment = 5000;
+Segment = 2000;
 
 r = -Width/2:1/Segment:Width/2;
 z_Dermis = -Depth_Dermis:1/Segment:0.0;
@@ -18,18 +18,18 @@ z = [z_Dermis, z_Fat, z_Muscle];
 [R,Z] = meshgrid(r,z);
 
 I0 = 2000.0; #[W/m^2], the irradiation intensity at the skin surface
-sigma = 0.002; #[m], the width of the irradiated region (the standard deviation of the Gaussian function which describes the beam profile)
-Ab_Dermis = 0.0012; #[1/m]
-Ab_Fat = 0.0009; #[1/m]
-Ab_Muscle = 0.003; #[1/m]
+sigma = 0.004; #[m], the width of the irradiated region (the standard deviation of the Gaussian function which describes the beam profile)
+Ab_Dermis = 12; #[1/m]
+Ab_Fat = 9; #[1/m]
+Ab_Muscle = 30; #[1/m]
 
-Q_Dermis = Ab_Dermis*exp(Ab_Dermis*Z(1:length(z_Dermis),:)-R(1:length(z_Dermis),:).^2/(2*sigma^2));
-Q_Dermis = I0*Q_Dermis;
-Q_Fat = Ab_Fat*exp(Ab_Fat*Z(length(z_Dermis)+1:length(z_Dermis)+length(z_Fat),:)-R(length(z_Dermis)+1:length(z_Dermis)+length(z_Fat),:).^2/(2*sigma^2));
-Q_Fat = I0*Q_Fat;
-Q_Muscle = Ab_Muscle*exp(Ab_Muscle*Z(length(z_Dermis)+length(z_Fat)+1:length(z_Dermis)+length(z_Fat)+length(z_Muscle),:)-R(length(z_Dermis)+length(z_Fat)+1:length(z_Dermis)+length(z_Fat)+length(z_Muscle),:).^2/(2*sigma^2));
-Q_Muscle = I0*Q_Muscle;
+Q_Dermis = I0*Ab_Dermis*exp(Ab_Dermis*Z(1:length(z_Dermis),:)-R(1:length(z_Dermis),:).^2/(2*sigma^2));
+Q_Fat = I0*Ab_Fat*exp(Ab_Fat*Z(length(z_Dermis)+1:length(z_Dermis)+length(z_Fat),:)-R(length(z_Dermis)+1:length(z_Dermis)+length(z_Fat),:).^2/(2*sigma^2));
+Q_Fat = Q_Fat - (Q_Fat(1,:)-Q_Dermis(size(Q_Dermis)(1),:));
+Q_Muscle = I0*Ab_Muscle*exp(Ab_Muscle*Z(length(z_Dermis)+length(z_Fat)+1:length(z_Dermis)+length(z_Fat)+length(z_Muscle),:)-R(length(z_Dermis)+length(z_Fat)+1:length(z_Dermis)+length(z_Fat)+length(z_Muscle),:).^2/(2*sigma^2));
+Q_Muscle = Q_Muscle - (Q_Muscle(1,:)-Q_Fat(size(Q_Fat)(1),:));
 Q = [Q_Dermis; Q_Fat; Q_Muscle];
 
-figure
+#figure
 mesh(R,Z,Q)
+
