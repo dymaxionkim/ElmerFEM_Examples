8d0dd88dd7653853137a531b07e345ea5170c8c2
dymaxionkim <dymaxion.kim@gmail.com>
2017-12-15 18:33:44 +0900

    Success

--- 20171213_FAT3_CAE/4.Elmer03/Beer.m
+++ 20171213_FAT3_CAE/4.Elmer03/Beer.m
@@ -1,3 +1,8 @@
+##########################################################
+# Beer-Lambert Plot
+# Heat Generation by LED Light through Human Skin Tissues
+# 20171215 Dymaxionkim
+##########################################################
 
 Depth_Dermis = 0.0015;
 Depth_Fat = 0.005;
@@ -5,34 +10,26 @@ Depth_Muscle = 0.020;
 Width = 0.06;
 Segment = 5000;
 
-r_Dermis = -Width/2:1/Segment:Width/2;
-r_Fat = r_Dermis;
-r_Muscle = r_Dermis;
-
+r = -Width/2:1/Segment:Width/2;
 z_Dermis = -Depth_Dermis:1/Segment:0.0;
 z_Fat = -(Depth_Dermis+Depth_Fat):1/Segment:-Depth_Dermis;
 z_Muscle = -(Depth_Dermis+Depth_Fat+Depth_Muscle):1/Segment:-(Depth_Dermis+Depth_Fat);
-
-[R_Dermis,Z_Dermis] = meshgrid(r_Dermis,z_Dermis);
-[R_Fat,Z_Fat] = meshgrid(r_Fat,z_Fat);
-[R_Muscle,Z_Muscle] = meshgrid(r_Muscle,z_Muscle);
+z = [z_Dermis, z_Fat, z_Muscle];
+[R,Z] = meshgrid(r,z);
 
 I0 = 2000.0; #[W/m^2], the irradiation intensity at the skin surface
 sigma = 0.002; #[m], the width of the irradiated region (the standard deviation of the Gaussian function which describes the beam profile)
-Absorptivity_Dermis = 0.0012; #[1/m]
-Absorptivity_Fat = 0.0009; #[1/m]
-Absorptivity_Muscle = 0.003; #[1/m]
+Ab_Dermis = 0.0012; #[1/m]
+Ab_Fat = 0.0009; #[1/m]
+Ab_Muscle = 0.003; #[1/m]
 
-Q_Dermis = I0*Absorptivity_Dermis*exp(Absorptivity_Dermis*Z_Dermis-R_Dermis.^2)./(2*sigma^2);
-Q_Fat = Absorptivity_Fat*exp(Absorptivity_Fat*Z_Fat-R_Fat.^2)./(2*sigma^2);
-Q_Fat = ones(size(Q_Fat)(1),size(Q_Fat)(2)).*Q_Dermis(1,:) .* Q_Fat
-Q_Muscle = I0*Absorptivity_Muscle*exp(Absorptivity_Muscle*Z_Muscle-R_Muscle.^2)./(2*sigma^2);
+Q_Dermis = Ab_Dermis*exp(Ab_Dermis*Z(1:length(z_Dermis),:)-R(1:length(z_Dermis),:).^2/(2*sigma^2));
+Q_Dermis = I0*Q_Dermis;
+Q_Fat = Ab_Fat*exp(Ab_Fat*Z(length(z_Dermis)+1:length(z_Dermis)+length(z_Fat),:)-R(length(z_Dermis)+1:length(z_Dermis)+length(z_Fat),:).^2/(2*sigma^2));
+Q_Fat = I0*Q_Fat;
+Q_Muscle = Ab_Muscle*exp(Ab_Muscle*Z(length(z_Dermis)+length(z_Fat)+1:length(z_Dermis)+length(z_Fat)+length(z_Muscle),:)-R(length(z_Dermis)+length(z_Fat)+1:length(z_Dermis)+length(z_Fat)+length(z_Muscle),:).^2/(2*sigma^2));
+Q_Muscle = I0*Q_Muscle;
+Q = [Q_Dermis; Q_Fat; Q_Muscle];
 
 figure
-mesh(R_Dermis,Z_Dermis,Q_Dermis)
-hold on;
-#figure
-mesh(R_Fat,Z_Fat,Q_Fat)
-#hold on;
-#figure
-#mesh(R_Muscle,Z_Muscle,Q_Muscle)
+mesh(R,Z,Q)
